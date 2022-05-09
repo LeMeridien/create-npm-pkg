100f08f0f6c0e8624df4a0b13fb0e292731990e7
thjiang <browserify@qq.com>
2022-05-09 22:31:38 +0800

    :sprarkles: feat: getTemplateInstallPackage

--- src/createNpmPackage.js
+++ src/createNpmPackage.js
@@ -133,4 +133,51 @@ const getInstallPackage = (version, originalDirectory) => {
 	return Promise.resolve(packageToInstall);
 };
 
+const getTemplateInstallPackage = (template, originalDirectory) => {
+	let templateToInstall = '@cru/npm-package-template';
+	if (template) {
+		if (template.match(/^file:/)) {
+			templateToInstall = `file:${path.resolve(
+				originalDirectory,
+				template.match(/^file:(.*)?$/)[1]
+			)}`;
+		} else if (
+			template.includes('://') ||
+			template.match(/^.+\.(tgz|tar\.gz)$/)
+		) {
+			// for tar.gz or alternative paths
+			templateToInstall = template;
+		} else {
+			// Add prefix 'cra-template-' to non-prefixed templates, leaving any
+			// @scope/ and @version intact.
+			const packageMatch = template.match(/^(@[^/]+\/)?([^@]+)?(@.+)?$/);
+			const scope = packageMatch[1] || '';
+			const templateName = packageMatch[2] || '';
+			const version = packageMatch[3] || '';
+
+			if (
+				templateName === templateToInstall ||
+				templateName.startsWith(`${templateToInstall}-`)
+			) {
+				// Covers:
+				// - cra-template
+				// - @SCOPE/cra-template
+				// - cra-template-NAME
+				// - @SCOPE/cra-template-NAME
+				templateToInstall = `${scope}${templateName}${version}`;
+			} else if (version && !scope && !templateName) {
+				// Covers using @SCOPE only
+				templateToInstall = `${version}/${templateToInstall}`;
+			} else {
+				// Covers templates without the `cra-template` prefix:
+				// - NAME
+				// - @SCOPE/NAME
+				templateToInstall = `${scope}${templateToInstall}-${templateName}${version}`;
+			}
+		}
+	}
+
+	return Promise.resolve(templateToInstall);
+};
+
 export { init };
